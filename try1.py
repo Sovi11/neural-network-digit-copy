bec109ceda31cae8369cb204f99bc0e61bbcd6ef
Sovi11 <pravarkataria@gmail.com>
2023-12-05 22:53:48 +0530

    adding files

---
+++ try1.py
@@ -0,0 +1,37 @@
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+import os 
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train) , (x_test,y_test) = mnist.load_data()
+
+
+x_train = tf.keras.utils.normalize(x_train, axis =1)
+x_test = tf.keras.utils.normalize(x_test, axis =1)
+
+model  = tf.keras.models.Sequential() 
+
+model.add(tf.keras.layers.Flatten(input_shape = (28,28)))
+# model.add(tf.keras.layers.Dense(326, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(32, activation='relu'))
+# model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy' , metrics =['accuracy'])
+
+model.fit(x_train,y_train,epochs = 10)
+
+model.save('handwritten_128_32_10.model')
+
+# model = tf.keras.models.load_model('handwritten.model')
+loss, accuracy  = model.evaluate(x_test, y_test)
+print(loss)
+print(accuracy)
+
+
+
+
